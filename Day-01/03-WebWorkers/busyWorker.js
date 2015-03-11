0de1b89213509530278c86ad9870ea4c75cef555
tkmagesh <tkmagesh77@gmail.com>
2015-03-11 17:07:06 +0530

    Day-1 Complete

--- Day-01/03-WebWorkers/busyWorker.js
+++ Day-01/03-WebWorkers/busyWorker.js
@@ -1,13 +1,21 @@
 function doWork(){
-    for(var i=0; i<10000;i++)
+    for(var i=1; i<=10000;i++){
         for(var j=0; j<10000;j++)
             for(var k=0; k<100;k++)
             {}
+        if (i % 100 === 0){
+            var data = {
+                type : "progress",
+                percentCompleted : (i / 100)
+            };
+            self.postMessage(data);
+        }
+    }
 }
 self.addEventListener("message", function(evtArg){
     if (evtArg.data === "start"){
         doWork();
-        self.postMessage("done");
+        self.postMessage({type : "done"});
     } else {
         console.log("unknown message", evtArg);
     }
